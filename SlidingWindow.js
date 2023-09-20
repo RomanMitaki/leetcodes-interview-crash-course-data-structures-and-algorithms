7f18e11fa923c60e349bd67ac7eec8aaed965c6a
Roman <r.mitaki@yandex.ru>
2023-09-20 14:58:03 +0300

    Create SlidingWindow.js

---
+++ SlidingWindow.js
@@ -0,0 +1,61 @@
+//Maximum Average Subarray I
+
+//You are given an integer array nums consisting of n elements, and an integer k.
+//Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+//Input: nums = [1,12,-5,-6,50,3], k = 4
+//Output: 12.75000
+//Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let currSum = 0;
+    let ans = 0;
+    for (let i = 0; i < k; i++) {
+        currSum += nums[i];
+    }
+    let currAverage = currSum / k;
+    ans = currAverage;
+    for (let i = k; i < nums.length; i++) {
+        currSum += nums[i] - nums[i - k];
+        currAverage = currSum / k;
+        ans = Math.max(ans, currAverage)
+    }
+    return ans;
+};
+
+//Max Consecutive Ones III
+
+//Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+//Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+//Output: 6
+//Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+//Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let left = 0, ans = 0, zeroes = 0;
+    
+    for (let curr = 0; curr < nums.length; curr++) {
+        if (nums[curr] === 0) {
+            zeroes++;
+        }
+        while (zeroes > k) {
+            if (nums[left] === 0) {
+                zeroes--;
+            }
+            left++;
+        }
+        ans = Math.max(ans, curr - left + 1);
+    }
+    return ans;
+};
