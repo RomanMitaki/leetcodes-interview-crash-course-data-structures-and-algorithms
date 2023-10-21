e521eff8763454fd30d9c1c44041be9f6da25d1b
Roman <r.mitaki@yandex.ru>
2023-10-21 15:33:39 +0300

    Update Greedy.js

--- Greedy.js
+++ Greedy.js
@@ -46,3 +46,62 @@ var partitionArray = function(nums, k) {
     }
     return ans;
 };
+
+//Least Number of Unique Integers after K Removals
+
+//https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
+
+//Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function(arr, k) {
+    const nums = new Map();
+    arr.forEach(num => nums.set(num, (nums.get(num) || 0) + 1));
+    const numsValues = [];
+    for (let val of nums.values()) {
+        numsValues.push(val)
+    }
+    numsValues.sort((a, b) => b - a);
+    while (k && numsValues.length) {
+        let num = numsValues[numsValues.length - 1];
+        if (num <= k) {
+            k -= num;
+            numsValues.pop();
+        } else {
+            break;
+        }
+    }
+    return numsValues.length;
+};
+
+//Boats to Save People
+
+//https://leetcode.com/problems/boats-to-save-people/
+
+//You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+//Return the minimum number of boats to carry every given person.
+
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    let ans = 0;
+    people.sort((a, b) => a - b);
+    let lightest = 0, heaviest = people.length - 1;
+    while (lightest <= heaviest) {
+        if (people[heaviest] + people[lightest] <= limit) {
+            heaviest--;
+            lightest++;
+        } else {
+            heaviest--;
+        }
+        ans++;
+    }
+    return ans;
+};
