b4b4c3a9c41fc7cbee672fa39c54f5f05746875d
Roman <r.mitaki@yandex.ru>
2023-10-25 20:40:35 +0300

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -266,3 +266,43 @@ var minimumEffortPath = function(heights) {
     return min;
 };
 
+//Minimum Speed to Arrive on Time
+
+//https://leetcode.com/problems/minimum-speed-to-arrive-on-time/description/
+
+//You are given a floating-point number hour, representing the amount of time you have to reach the office. To commute to the office, you must take n trains in sequential order. You are also given an integer array dist of length n, where dist[i] describes the distance (in kilometers) of the ith train ride.
+//Each train can only depart at an integer hour, so you may need to wait in between each train ride.
+//For example, if the 1st train ride takes 1.5 hours, you must wait for an additional 0.5 hours before you can depart on the 2nd train ride at the 2 hour mark.
+//Return the minimum positive integer speed (in kilometers per hour) that all the trains must travel at for you to reach the office on time, or -1 if it is impossible to be on time.
+//Tests are generated such that the answer will not exceed 107 and hour will have at most two digits after the decimal point.
+
+/**
+ * @param {number[]} dist
+ * @param {number} hour
+ * @return {number}
+ */
+var minSpeedOnTime = function(dist, hour) {
+    const checkTime = (speed) => {
+        let time = 0;
+        for (let distance of dist) {
+            time = Math.ceil(time);
+            time += distance / speed;
+        }
+        return time <= hour;
+    }
+
+    if (Math.ceil(hour) < dist.length) return -1;
+
+    let min = 1;
+    let max = 10 ** 7;
+
+    while (min <= max) {
+        let mid = Math.floor((min + max) / 2);
+        if (checkTime(mid)) {
+            max = mid - 1;
+        } else {
+            min = mid + 1;
+        }
+    }
+    return min;
+}
