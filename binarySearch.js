a158c0a389290652ab1dff8a3743391c8e800458
Roman <r.mitaki@yandex.ru>
2023-10-26 21:23:00 +0300

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -306,3 +306,40 @@ var minSpeedOnTime = function(dist, hour) {
     }
     return min;
 }
+
+//Find the Smallest Divisor Given a Threshold
+
+//https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/description/
+
+//Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
+//Each result of the division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
+//The test cases are generated so that there will be an answer.
+
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+var smallestDivisor = function(nums, threshold) {
+    const checkDivisor = (divisor) => {
+        let sum = 0;
+        for (let num of nums) {
+            sum += Math.ceil(num / divisor);
+        }
+        return sum <= threshold;
+    }
+    
+    let left = 1;
+    let right = Math.max(...nums);
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (checkDivisor(mid)) {
+            right = mid - 1;
+        }  else {
+            left = mid + 1;
+        }
+    }
+    
+    return left;
+};
