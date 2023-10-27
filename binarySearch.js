67c3d3231ae0f455c7b1998f3549d2778573fbfa
Roman <r.mitaki@yandex.ru>
2023-10-27 19:58:08 +0300

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -343,3 +343,109 @@ var smallestDivisor = function(nums, threshold) {
     
     return left;
 };
+
+//Guess Number Higher or Lower
+
+//https://leetcode.com/problems/guess-number-higher-or-lower/
+
+//We are playing the Guess Game. The game is as follows:
+//I pick a number from 1 to n. You have to guess which number I picked.
+//Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+//You call a pre-defined API int guess(int num), which returns three possible results:
+//-1: Your guess is higher than the number I picked (i.e. num > pick).
+//1: Your guess is lower than the number I picked (i.e. num < pick).
+//0: your guess is equal to the number I picked (i.e. num == pick).
+//Return the number that I picked.
+
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    let left = 0, right = n;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (!guess(mid)) return mid;
+        if (guess(mid) === -1) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+};
+
+//Maximum Distance Between a Pair of Values
+
+//https://leetcode.com/problems/maximum-distance-between-a-pair-of-values/
+
+//You are given two non-increasing 0-indexed integer arrays nums1​​​​​​ and nums2​​​​​​.
+//A pair of indices (i, j), where 0 <= i < nums1.length and 0 <= j < nums2.length, is valid if both i <= j and nums1[i] <= nums2[j]. The distance of the pair is j - i​​​​.
+//Return the maximum distance of any valid pair (i, j). If there are no valid pairs, return 0.
+//An array arr is non-increasing if arr[i-1] >= arr[i] for every 1 <= i < arr.length.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDistance = function(nums1, nums2) {
+    let ans = 0;
+    for (let i = 0; i < nums1.length; i++) {
+        let left = i, right = nums2.length - 1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (nums2[mid] < nums1[i]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        ans = Math.max(ans, right - i)
+    }
+    return ans
+};
+
+//Minimum Time to Complete Trips
+
+//https://leetcode.com/problems/minimum-time-to-complete-trips/description/
+
+//You are given an array time where time[i] denotes the time taken by the ith bus to complete one trip.
+//Each bus can make multiple trips successively; that is, the next trip can start immediately after completing the current trip. Also, each bus operates independently; that is, the trips of one bus do not influence the trips of any other bus.
+//You are also given an integer totalTrips, which denotes the number of trips all buses should make in total. Return the minimum time required for all buses to complete at least totalTrips trips.
+
+/**
+ * @param {number[]} time
+ * @param {number} totalTrips
+ * @return {number}
+ */
+var minimumTime = function(time, totalTrips) {
+    let left = 1, right = Math.pow(10, 15);
+    const checkTime = (t) => {
+        let total = 0;
+        for (let s of time) {
+            total += Math.floor(t / s);
+        }
+        return total >= totalTrips
+    }
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (checkTime(mid)) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+};
