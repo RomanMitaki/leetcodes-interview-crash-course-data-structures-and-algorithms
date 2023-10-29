46d27b1f943f545e54dd3baf8e97d686b5b384e1
Roman <r.mitaki@yandex.ru>
2023-10-29 14:16:09 +0300

    Update backtracking.js

--- backtracking.js
+++ backtracking.js
@@ -27,3 +27,63 @@ var permute = function(nums) {
     backtrack([]);
     return ans;
 };
+
+//Subsets
+
+//https://leetcode.com/problems/subsets/
+
+//Given an integer array nums of unique elements, return all possible subsets (the power set).
+//The solution set must not contain duplicate subsets. Return the solution in any order.\
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    const ans = [];
+    const backtrack = (curr, i) => {
+        if (i > nums.length) {
+            return
+        }
+
+        ans.push([...curr]);
+        for (let j = i; j < nums.length; j++) {
+            curr.push(nums[j]);
+            backtrack(curr, j + 1)
+            curr.pop();
+        }
+    }
+
+    backtrack([], 0);
+    return ans;
+};
+
+//Combinations
+
+//https://leetcode.com/problems/combinations/description/
+
+//Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+//You may return the answer in any order.
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    const ans = [];
+    const backtrack = (curr, i) => {
+        if (curr.length === k) {
+            ans.push([...curr]);
+            return
+        }
+
+        for (let j = i; j <= n; j++) {
+            curr.push(j);
+            backtrack(curr, j + 1);
+            curr.pop();
+        }
+    }
+    backtrack([], 1);
+    return ans;
+};
