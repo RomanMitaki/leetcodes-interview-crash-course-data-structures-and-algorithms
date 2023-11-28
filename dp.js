7b9fcb510b514b126954a389872ba1c5e80bcf70
Roman <r.mitaki@yandex.ru>
2023-11-28 21:32:05 +0300

    Update dp.js

--- dp.js
+++ dp.js
@@ -21,3 +21,31 @@ var minCostClimbingStairs = function(cost) {
     let memo = new Map();
     return dp(cost.length);
 };
+
+//House Robber
+
+//https://leetcode.com/problems/house-robber/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    const dp = (i) => {
+        if (i === 1) {
+            return nums[0]
+        }
+       
+        let backTwo = nums[0];
+        let backOne = Math.max(nums[0], nums[1]);
+
+        for (let j = 2; j < i; j++) {
+            let temp = backOne;
+            backOne = Math.max(backOne, backTwo + nums[j]);
+            backTwo = temp;
+        }
+
+        return backOne;
+    }
+    return dp(nums.length)
+};
