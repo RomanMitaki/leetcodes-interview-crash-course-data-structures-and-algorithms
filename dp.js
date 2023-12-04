ca7c6a62460d80b8c6c4ee48d979c8aef562056f
Roman <r.mitaki@yandex.ru>
2023-12-04 22:02:12 +0300

    Update dp.js

--- dp.js
+++ dp.js
@@ -68,3 +68,21 @@ var lengthOfLIS = function(nums) {
     }
     return Math.max(...ans)
 };
+
+//Climbing Stairs
+
+//https://leetcode.com/problems/climbing-stairs/description/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let dp = new Array(n).fill(0);
+    dp[0] = 1;
+    dp[1] = 2;
+    for (let i = 2; i < n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n-1]
+};
