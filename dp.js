1cd9ed339d4c6d6737ee254349e933d01f1603e0
Roman <r.mitaki@yandex.ru>
2023-12-06 22:15:52 +0300

    Update dp.js

--- dp.js
+++ dp.js
@@ -104,3 +104,26 @@ var minCostClimbingStairs = function(cost) {
     }
     return Math.min(dp[len-1], dp[len-2])
 };
+
+//Coin Change
+//https://leetcode.com/problems/coin-change/description/
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    const dp = new Array(amount + 1).fill(amount + 1);
+    dp[0] = 0;
+
+    for (let i = 1; i < dp.length; i++) {
+        for (let coin of coins) {
+            if (i - coin < 0) continue;
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        }
+    }
+
+    return dp[amount] === amount + 1 ? -1 : dp[amount];
+
+};
