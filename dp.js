c5753153ec165545d400037d08fa3790e9a6da9b
Roman <r.mitaki@yandex.ru>
2023-12-13 11:19:54 +0300

    Update dp.js

--- dp.js
+++ dp.js
@@ -160,3 +160,30 @@ var longestCommonSubsequence = function(text1, text2) {
     }
     return dp(0, 0);
 };
+
+//Unique Paths
+//https://leetcode.com/problems/unique-paths/description/
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const dp = [];
+    for (let i = 0; i < m; i++) {
+        dp.push(new Array(n).fill(0));
+    }
+    dp[0][0] = 1;
+    for (let row = 0; row < m; row++) {
+        for (let col = 0; col < n; col++) {
+            if (row > 0) {
+                dp[row][col] += dp[row - 1][col];
+            }
+            if (col > 0) {
+                dp[row][col] += dp[row][col - 1];
+            }
+        }
+    }
+    return dp[m - 1][n - 1]
+};
